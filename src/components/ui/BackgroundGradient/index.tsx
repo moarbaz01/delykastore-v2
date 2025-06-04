39fffd789e04819ed65016711405c1a66799e216
Arbaz Abr <arbazmr123@gmail.com>
2025-06-04 13:23:07 +0530

    changed animation style

--- src/components/ui/BackgroundGradient/index.tsx
+++ src/components/ui/BackgroundGradient/index.tsx
@@ -11,22 +11,20 @@ export const BackgroundGradient = ({
   className?: string;
   containerClassName?: string;
 }) => {
-  // Same mixed radial gradient
-  const gradientClass =
-    "bg-[radial-gradient(circle_farthest-side_at_0_100%,#ff4d4d,transparent),radial-gradient(circle_farthest-side_at_100%_0,#4b0082,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#1e3a8a,transparent),radial-gradient(circle_farthest-side_at_0_0,#990000,#0f172a)]";
-
   return (
     <div className={cn("relative p-[4px] group", containerClassName)}>
       <div
         className={cn(
-          "absolute inset-0 rounded-3xl z-[1] opacity-60 group-hover:opacity-100 blur-xl transition duration-500",
-          gradientClass
+          "absolute inset-0 rounded-3xl z-[1] blur-xl opacity-60 ",
+          "animate-gradient bg-[length:400%_400%]",
+          "bg-[radial-gradient(circle_farthest-side_at_0_100%,#ff4d4d,transparent),radial-gradient(circle_farthest-side_at_100%_0,#4b0082,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#1e3a8a,transparent),radial-gradient(circle_farthest-side_at_0_0,#990000,#0f172a)]"
         )}
       />
-      <div
+      <div             
         className={cn(
           "absolute inset-0 rounded-3xl z-[1]",
-          gradientClass
+          "animate-gradient bg-[length:400%_400%]",
+          "bg-[radial-gradient(circle_farthest-side_at_0_100%,#ff4d4d,transparent),radial-gradient(circle_farthest-side_at_100%_0,#4b0082,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#1e3a8a,transparent),radial-gradient(circle_farthest-side_at_0_0,#990000,#0f172a)]"
         )}
       />
       <div className={cn("relative z-10", className)}>{children}</div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,25 @@ export default {
   ],
   theme: {
     extend: {
+      animation: {
+        gradient: "gradientBG 8s ease infinite",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
         primary: "#f68181",
         secondary: "#989797",
         accent: "#d4a158",
+      },
+      keyframes: {
+        gradientBG: {
+          "0%, 100%": {
+            backgroundPosition: "0% 50%",
+          },
+          "50%": {
+            backgroundPosition: "100% 50%",
+          },
+        },
       },
     },
   },
